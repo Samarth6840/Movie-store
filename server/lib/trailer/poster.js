e53a5c74e45a556172e721a89db243664259f7a1
Samarth Joshi <samarth54922@gmail.com>
2026-08-26 22:25:17 +0530

    Skip grade in poster rendering too

--- server/lib/trailer/poster.js
+++ server/lib/trailer/poster.js
@@ -1,30 +1,16 @@
 
 import { createFrame } from '../paint/frame.js';
-import { bakeGrade, applyGrade, applyAberration, letterbox } from '../paint/grade.js';
+import { letterbox } from '../paint/grade.js';
 import { buildScene } from '../scene/scene.js';
 import { renderStaticText } from './animator.js';
-import { GRADES } from '../paint/grade.js';
 
 export const renderPoster = (script, ctx, width, height) => {
   const frame = createFrame(width, height);
   const firstShot = script.shots[0];
 
-  
   const scene = buildScene(firstShot.scene, ctx.at('scene', 0), width, height);
   scene.draw(frame, 0.4, 1);
 
-  
-  const grade = GRADES.find((g) => g.name === script.grade) ?? GRADES[0];
-  const gradeTable = bakeGrade(grade);
-  applyGrade(frame, gradeTable, {
-    saturation: grade.saturation,
-    vignette: 0.4,
-    grain: 0.03,
-    frameIndex: 0,
-    exposure: 1.05,
-  });
-
-  
   letterbox(frame, 2.39);
 
   return frame;
